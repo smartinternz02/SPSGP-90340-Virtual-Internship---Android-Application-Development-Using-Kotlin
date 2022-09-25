e614f6733866b01683b3f66b42e59317fb81d193
RGAK637 <81611345+RGAK637@users.noreply.github.com>
2022-09-25 18:47:55 +0530

    Delete grocery-app directory

--- grocery-app/Grocery app project-2/app/src/main/java/com/abhi/grocerylist/ui/shoppinglist/AddDialogListener.kt
+++
@@ -1,7 +0,0 @@
-package com.abhi.grocerylist.ui.shoppinglist
-
-import com.abhi.grocerylist.data.db.entities.ShoppingItem
-
-interface AddDialogListener {
-    fun onAddButtonClicked(item: ShoppingItem)
-}
